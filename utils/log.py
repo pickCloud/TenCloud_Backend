3c3e8932777445b47642b5e6921d02fef6d22d59
Jon <hongjiongteng@gmail.com>
2017-05-12 15:49:29 +0800

    log模块变成通用

--- utils/log.py
+++ utils/log.py
@@ -2,18 +2,19 @@ __author__ = 'Jon'
 
 '''
 log文件
+    usage::
+    >>> from log import LOG
+    >>> LOG.debug('test debug')
+    >>> LOG.info('test info')
+    >>> LOG.warn('test warn')
+    >>> LOG.error('test error')
+    >>> LOG.stats('test stats')
 '''
 
 import os
 import sys
-import time
-import socket
 import logging
 from logging.handlers import TimedRotatingFileHandler
-
-from tornado.options import options
-from setting import settings
-from utils.datetool import seconds_to_human
 
 
 # 新增level, 用于统计
@@ -28,23 +29,25 @@ class ExactLogLevelFilter(logging.Filter):
         return log_record.levelno == self.__level
 
 
-class Log(object):
+class TenLogService():
     def __init__(self, name='Dashboard',
                        path='logs/',
                        when='D',
                        backcount=7,
                        fmt='[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d] %(message)s',
-                       datefmt='%y%m%d %H:%M:%S'):
+                       datefmt='%y%m%d %H:%M:%S',
+                       log_level='DEBUG'):
         '''
-        :param name: logger name
-        :param path: 目录, 默认当前目录的logs
-        :param when: 备份时间, 默认一天备份一次
+        :param name:      logger name
+        :param path:      目录, 默认当前目录的logs
+        :param when:      备份时间, 默认一天备份一次
         :param backcount: 备份数, 默认只备份7个文件, 即只保留一礼拜的log
-        :param fmt: logging.Formatter的fmt, e.g. [W 161212 16:23:47 log:95] message
-        :param datefmt: logging.Formatter的datefmt, e.g. 161212 16:23:47
+        :param fmt:       logging.Formatter的fmt, e.g. [W 161212 16:23:47 log:95] message
+        :param datefmt:   logging.Formatter的datefmt, e.g. 161212 16:23:47
+        :param log_level: 日记级别 e.g. DEBUG
         '''
         self.__logger = logging.getLogger(name)
-        self.__logger.setLevel(settings['log_level'])
+        self.__logger.setLevel(log_level)
 
         self._handlers = dict()
         self._backcount = backcount
@@ -57,7 +60,7 @@ class Log(object):
             'INFO':     os.path.join(path, 'info/info.log'),
             'WARN':     os.path.join(path, 'warn/warn.log'),
             'ERROR':    os.path.join(path, 'error/error.log'),
-            'STATS':     os.path.join(path, 'stats/stats.log')
+            'STATS':    os.path.join(path, 'stats/stats.log')
         }
         self._create_handlers()
 
@@ -103,37 +106,11 @@ class Log(object):
 
         self.__logger.error(msg)
 
-    def stats(self, _self, extra=None):
-        data = {
-            'req_time': seconds_to_human(_self._req_time),
-            'hostname': socket.gethostname(),
-            'appid':    settings['hr_infra_appid'],
-            'http_code': _self.get_status(),
-            'status_code': 0,
-            'opt_time': '%.2f' % ((time.time() - _self._req_time)*1000),
-            'res_type': 'json',
-            'req_uri':   _self.request.uri,
-            'req_type':  _self.request.method,
-            'referer':   _self.request.headers.get('Referer'),
-            'remote_ip': _self.request.headers.get('X-Real-IP') or _self.request.remote_ip,
-            'useragent': _self.request.headers.get('User-Agent'),
-            'event': '{handler}-{method}'.format(handler=_self.__module__ + '.' + _self.__class__.__name__, method=_self.request.method)
-        }
-
-        if extra:
-            data.update(extra)
-
-        msg = self._wrap_msg(str(data))
+    def stats(self, msg):
+        '''用户行为日志, msg可以是dict/list
+        '''
+        msg = self._wrap_msg(str(msg))
 
         self.__logger.log(logging.getLevelName('STATS'), msg)
 
-LOG = Log()
-
-if __name__ == '__main__':
-    LOG = Log(path=options.logpath)
-
-    LOG.debug('test debug')
-    LOG.info('test info')
-    LOG.warn('test warn')
-    LOG.error('test error')
-    LOG.stats('test stats')
+LOG = TenLogService()
