38f3e55ae6d085e3f017af5827913197c77fd9b4
Jon <hongjiongteng@gmail.com>
2017-05-16 15:29:32 +0800

    修过README

---
+++ constant.py
@@ -0,0 +1,7 @@
+__author__ = 'Jon'
+
+'''
+程序的常量
+'''
+
+CLUSTER_DATE_FORMAT =  '%Y年%m月%d日'
--- service/cluster/cluster.py
+++ service/cluster/cluster.py
@@ -2,6 +2,7 @@ __author__ = 'Jon'
 
 from tornado.gen import coroutine
 from service.base import BaseService
+from constant import CLUSTER_DATE_FORMAT
 
 
 class ClusterService(BaseService):
@@ -14,7 +15,21 @@ class ClusterService(BaseService):
         cur = yield self.db.execute(sql)
         data = cur.fetchall()
 
-        return data
+        return self._change_date_format(data)
+
+    def _change_date_format(self, data):
+        '''
+        :param data: e.g. ((, , , , datetime.datetime(2017, 5, 16, 10, 24, 31), datetime.datetime(2017, 5, 16, 10, 27, 27)),)
+        :return:     e.g. ((, , , , '2017年05月16日', '2017年05月16日'),)
+        '''
+        result = []
+
+        for d in data:
+            row = list(d)
+            row[4], row[5] = row[4].strftime(CLUSTER_DATE_FORMAT), row[5].strftime(CLUSTER_DATE_FORMAT)
+            result.append(row)
+
+        return result
 
     @coroutine
     def add_cluster(self, params):
