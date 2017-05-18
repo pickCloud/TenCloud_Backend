070b1d04f4ed7a6ddf17e70908b513c754579488
Jon <hongjiongteng@gmail.com>
2017-05-18 09:27:42 +0800

    change db cursorclass

--- utils/db.py
+++ utils/db.py
@@ -1,7 +1,7 @@
 __author__ = 'Jon'
 
 from setting import settings
-from tornado_mysql import pools
+from tornado_mysql import pools, cursors
 
 DB = pools.Pool(
         dict(host=settings['mysql_host'],
@@ -9,6 +9,7 @@ DB = pools.Pool(
              user=settings['mysql_user'],
              passwd=settings['mysql_password'],
              db=settings['mysql_database'],
+             cursorclass=cursors.DictCursor,
              charset=settings['mysql_charset']),
         max_idle_connections=16,
         max_recycle_sec=120
