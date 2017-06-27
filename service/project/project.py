b77c697279c504fa9d35424191bd132bc1ea5bb2
Jon <hongjiongteng@gmail.com>
2017-06-27 14:02:16 +0800

    run_on_executor

--- service/project/project.py
+++ service/project/project.py
@@ -1,6 +1,6 @@
 __author__ = 'Jon'
 
-from tornado.gen import coroutine
+from tornado.concurrent import run_on_executor
 from service.base import BaseService
 from constant import CREATE_IMAGE_CMD
 
@@ -9,7 +9,7 @@ class ProjectService(BaseService):
     table  = 'project'
     fields = 'id, name, description, repos_name, repos_url'
 
-    @coroutine
+    @run_on_executor
     def create_image(self, params):
         '''
         :param params: dict e.g. {'prj_name': str, 'repos_url': str, 'branch_name': str, 'public_ip': str, 'username': str, 'passwd': str}
