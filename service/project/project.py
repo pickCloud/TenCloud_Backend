bf4afa78d492199139237aee56651f7738d5a74a
lanarthur <lancelot.hga@gmail.com>
2017-06-30 15:01:25 +0800

    代码格式

--- service/project/project.py
+++ service/project/project.py
@@ -7,35 +7,35 @@ from setting import settings
 
 
 class ProjectService(BaseService):
-	table = 'project'
-	fields = 'id, name, description, repos_name, repos_url'
+    table = 'project'
+    fields = 'id, name, description, repos_name, repos_url'
 
-	@coroutine
-	def create_image (self, params):
-		'''
+    @coroutine
+    def create_image(self, params):
+        '''
         :param params: dict e.g. {'prj_name': str, 'repos_url': str, 'branch_name': str, 'public_ip': str, 'username': str, 'passwd': str}
         '''
 
-		cmd = CREATE_IMAGE_CMD + ' '.join([params['prj_name'], params['repos_url'], params['branch_name']])
+        cmd = CREATE_IMAGE_CMD + ' '.join([params['prj_name'], params['repos_url'], params['branch_name']])
 
-		yield self.remote_ssh(params, cmd)
+        yield self.remote_ssh(params, cmd)
 
-	@coroutine
-	def deployment (self, params):
-		image_name = REPOS_DOMAIN + "/library/" + params['image_name']
-		cmd = DEPLOY_CMD.format(
-				repository=REPOS_DOMAIN,
-				username=settings['deploy_username'],
-				password=settings['deploy_password'],
-				image_name=image_name)
-		yield self.remote_ssh(params, cmd)
+    @coroutine
+    def deployment(self, params):
+        image_name = REPOS_DOMAIN + "/library/" + params['image_name']
+        cmd = DEPLOY_CMD.format(
+                repository=REPOS_DOMAIN,
+                username=settings['deploy_username'],
+                password=settings['deploy_password'],
+                image_name=image_name)
+        yield self.remote_ssh(params, cmd)
 
-	@coroutine
-	def find_image (self, params):
-		"""
+    @coroutine
+    def find_image(self, params):
+        """
         查找项目镜像
         """
-		cmd = IMAGE_INFO_CMD % (params['prj_name'])
-		out, err = yield self.remote_ssh(params, cmd)
-		data = [i for i in out]
-		return data, err
+        cmd = IMAGE_INFO_CMD % (params['prj_name'])
+        out, err = yield self.remote_ssh(params, cmd)
+        data = [i for i in out]
+        return data, err
