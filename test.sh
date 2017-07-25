86ab46500854bc9377c05c83a7a09470a9b5325a
lanarthur <lancelot.hga@gmail.com>
2017-07-25 14:26:47 +0800

    dont follow test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-branch="dev_hongguoan"
-current_branch=$(git branch 2>/dev/null | grep "^\*" | sed -e "s/^\*\ //")
-if [ ${current_branch} != ${branch} ];then
-    git checkout "${branch}"
-    echo "checkout to dev_hongguoan"
-fi
-echo "current branch is dev_hongguoan"
