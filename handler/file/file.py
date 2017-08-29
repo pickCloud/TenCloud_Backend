e546d017c47a72d1fa46b220d42ebd7594b1bc91
lanarthur <lancelot.hga@gmail.com>
2017-08-29 10:51:39 +0800

    fix batch deal bug

--- handler/file/file.py
+++ handler/file/file.py
@@ -232,7 +232,7 @@ class FileDownloadHandler(BaseHandler):
         try:
             sym = ("%s, " * len(self.params['file_ids'])).rstrip(' ,')
             arg = "id in (" + sym + ")"
-            data = yield self.file_service.select(fields='qiniu_id', conds=[arg], params=[self.params['file_ids']], ut=False, ct=False, one=True)
+            data = yield self.file_service.select(fields='qiniu_id', conds=[arg], params=self.params['file_ids'], ut=False, ct=False, one=True)
             urls = []
             for i in data:
                 url = yield self.file_service.private_download_url(qiniu_id=i['qiniu_id'])
@@ -310,7 +310,7 @@ class FileDeleteHandler(BaseHandler):
         try:
             sym = ("%s, " * len(self.params['file_ids'])).rstrip(' ,')
             arg = "id in (" + sym + ")"
-            yield self.file_service.delete(conds=[arg], params=[self.params['file_ids']])
+            yield self.file_service.delete(conds=[arg], params=self.params['file_ids'])
         except:
             self.error()
             self.log.error(traceback.format_exc())
