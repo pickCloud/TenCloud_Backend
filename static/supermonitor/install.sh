01f81fe60c750f5dee6cbfc909ece349ef10eabc
lookinghong <hongjiongteng@gmail.com>
2017-09-13 14:27:39 +0800

    add sudo

--- static/supermonitor/install.sh
+++ static/supermonitor/install.sh
@@ -10,7 +10,7 @@ DownloadFunc() {
     name=${1}
     url=${2}
     stroage=${3}
-  curl --retry 3 --retry-delay 2 -s -L -o ${stroage} ${url}
+  sudo curl --retry 3 --retry-delay 2 -s -L -o ${stroage} ${url}
   ret=$?
   if [ ${ret} -ne 0 ];then
     echoerr "failed to download ${name}"
@@ -24,54 +24,54 @@ report_data="report_data.service"
 report_data_url=${base_url}${report_data}
 report_data_stroage="/etc/systemd/system/report_data.service"
 if [ -f ${report_data_stroage} ];then
-    systemctl stop ${report_data}
-    systemctl disable ${report_data}
-    rm ${report_data_stroage}
+    sudo systemctl stop ${report_data}
+    sudo systemctl disable ${report_data}
+    sudo rm ${report_data_stroage}
 fi
 DownloadFunc ${report_data} ${report_data_url} ${report_data_stroage}
-chmod 644 ${report_data_stroage}
+sudo chmod 644 ${report_data_stroage}
 
 sync_linux_amd64="sync_linux_amd64"
 sync_url=${base_url}${sync_linux_amd64}
 sync_storage="/usr/sbin/sync_linux_amd64"
 if [ -f ${sync_storage} ];then
-    rm ${sync_storage}
+    sudo rm ${sync_storage}
 fi
 DownloadFunc ${sync_linux_amd64} ${sync_url} ${sync_storage}
-chmod 755 ${sync_storage}
+sudo chmod 755 ${sync_storage}
 pid=$(pgrep -f ${sync_linux_amd64})
 if pgrep -f ${sync_linux_amd64} > /dev/null; then
     echo "kill old version sync"
-    kill ${pid}
+    sudo kill ${pid}
 fi
 echo "create sync"
 log="/var/log/report_data"
 if [ ! -d ${log} ];then
-    mkdir -p ${log}
+    sudo mkdir -p ${log}
 fi
 
 clean_log_timer="clean-log-daily.timer"
 clean_log_timer_url=${base_url}${clean_log_timer}
 clean_log_timer_storage="/etc/systemd/system/clean-log-daily.timer"
 if [ -f ${clean_log_timer_storage} ];then
-    systemctl stop ${clean_log_timer}
-    systemctl disable ${clean_log_timer}
-    rm ${clean_log_timer_storage}
+    sudo systemctl stop ${clean_log_timer}
+    sudo systemctl disable ${clean_log_timer}
+    sudo rm ${clean_log_timer_storage}
 fi
 DownloadFunc ${clean_log_timer} ${clean_log_timer_url} ${clean_log_timer_storage}
-chmod 644 ${clean_log_timer_storage}
+sudo chmod 644 ${clean_log_timer_storage}
 
 clean_log_service="clean-log-daily.service"
 clean_log_service_url=${base_url}${clean_log_service}
 clean_log_service_storage="/etc/systemd/system/clean-log-daily.service"
 if [ -f ${clean_log_service_storage} ];then
-    rm ${clean_log_service_storage}
+    sudo rm ${clean_log_service_storage}
 fi
 DownloadFunc ${clean_log_service} ${clean_log_service_url} ${clean_log_service_storage}
-chmod 644 ${clean_log_service_storage}
+sudo chmod 644 ${clean_log_service_storage}
 
-systemctl daemon-reload
-systemctl enable ${report_data}
-systemctl start ${report_data}
-systemctl enable ${clean_log_timer}
-systemctl start ${clean_log_timer}
+sudo systemctl daemon-reload
+sudo systemctl enable ${report_data}
+sudo systemctl start ${report_data}
+sudo systemctl enable ${clean_log_timer}
+sudo systemctl start ${clean_log_timer}
