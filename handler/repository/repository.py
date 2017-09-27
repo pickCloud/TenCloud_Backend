2d2051acfc75938201be75e8d876e8db45fb9cce
lanarthur <lancelot.hga@gmail.com>
2017-09-27 17:03:45 +0800

    fix url bug

--- handler/repository/repository.py
+++ handler/repository/repository.py
@@ -39,7 +39,8 @@ class RepositoryHandler(BaseHandler):
         try:
             token = yield Task(self.redis.hget, GIT_TOKEN, self.current_user['id'])
             if not token:
-                url = yield self.repos_service.auth_callback()
+                original_path = self.request.protocol + "://" + self.request.host + self.request.uri
+                url = yield self.repos_service.auth_callback(original_path)
                 self.error(message='Require token!', code=401, data={'url': url})
                 return
 
