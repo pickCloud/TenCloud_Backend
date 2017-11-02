0f8a6e54a17735952e296524a978ee8018dbad02
lanarthur <lancelot.hga@gmail.com>
2017-11-02 15:44:50 +0800

    shield mobile reset

--- route.py
+++ route.py
@@ -103,7 +103,7 @@ routes = [
     (r'/api/user/captcha', GetCaptchaHandler),
     (r'/api/user/register', UserRegisterHandler),
     (r'/api/user/reset', UserResetPasswordHandler),
-    (r'/api/user/mobile/reset', UserResetMobileHandler),
+    # (r'/api/user/mobile/reset', UserResetMobileHandler),
 
     #文件上传
     (r'/api/file/upload', FileUploadHandler),
