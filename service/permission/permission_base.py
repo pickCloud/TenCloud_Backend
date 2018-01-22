d43d62eabab0b93d0ef56dc78d1549321f5283d2
lanarthur <lancelot.hga@gmail.com>
2018-01-22 12:25:56 +0800

    change pt format

--- service/permission/permission_base.py
+++ service/permission/permission_base.py
@@ -77,14 +77,23 @@ class PermissionBaseService(BaseService):
                 result[column['group']] = [tmp]
             else:
                 result[column['group']].append(tmp)
+        temp_data = {
+            'name': '企业管理',
+            'data': []
+        }
         for k in result:
+            # 企业资料，员工管理，权限模版划分为企业管理临时方案
             tmp_dict = {
                 'name': PERMISSIONS[k],
                 'data': [
                     {'name': PERMISSIONS[k], 'data': result[k]}
                 ]
             }
+            if k in [3, 4, 5]:
+                temp_data['data'].append(tmp_dict)
+                continue
             res.append(tmp_dict)
+        res.append(temp_data)
         return res
 
     @coroutine
