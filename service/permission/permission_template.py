1921e336f92bffac39cca0f3c5cf1041c8f24ef5
lanarthur <lancelot.hga@gmail.com>
2018-01-25 18:37:53 +0800

    rename

--- service/permission/permission_template.py
+++ service/permission/permission_template.py
@@ -172,7 +172,7 @@ class PermissionTemplateService(PermissionBaseService):
         files = ','.join([str(i['id']) for i in data['files']])
         projects = ','.join([str(i['id']) for i in data['projects']])
         data = {
-            'name': 'admin',
+            'name': '所有权限',
             'cid': cid,
             'permissions': permissions,
             'access_servers': servers,
