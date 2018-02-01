fc9aa441b98c837c4ddddf249d98284684eb77f0
lancelot <lancelot.hga@gmail.com>
2018-02-01 18:30:53 +0800

    remove add company bug

--- handler/company/company.py
+++ handler/company/company.py
@@ -111,6 +111,7 @@ class CompanyNewHandler(BaseHandler):
 
             # 创建公司
             self.params.pop('cid', None)
+            self.params.pop('token', None)
             info = yield self.company_service.add(self.params)
             yield self.company_employee_service.add(dict(cid=info['id'], uid=self.current_user['id'], status=APPLICATION_STATUS['founder'], is_admin=1))
             data = {
