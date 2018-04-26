f90d181a1939c97ca2354484af4f6b36f5277b0b
ShoreCN <c.shore.cn@gmail.com>
2018-04-26 11:39:05 +0800

    Modify the database table '_image' to 'image'
    Modify the database table '_image' to 'image'

--- service/imagehub/image.py
+++ service/imagehub/image.py
@@ -2,7 +2,7 @@
 from service.base import BaseService
 
 class ImageService(BaseService):
-    table = '_image'
+    table = 'image'
     fields = """
                 id, name, version, type, url, description, labels, logo_url, commit,
                 state, dockerfile, repos_name, repos_url, app_id, log, lord, form
